655de80aa22177872e0025b524e365a7f7edc2b1
Anthony Elijah <lionnet.ant@gmail.com>
2018-06-11 20:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// function without prm
+// function return string without prm
 function happyHolidays () {
   return 'Happy holidays!'
 }
 
-// function with one prm
+// function return string with one prm
 function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`
 }
 
-// function with two prm
+// function return string with two prm
 function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-// second function without prm
+// second return string function without prm
 function holidayCountdown (holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
